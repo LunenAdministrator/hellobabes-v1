419e2d2a350546c8e84c5af805db7d5d7673d205
Lunen Developer <71112248+LunenAdministrator@users.noreply.github.com>
2021-06-05 11:48:28 +0800

    Update version3.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -155,7 +155,7 @@
 				<div class="sliderAx h-5/6 px-2 py-5">
 					<div id="slider-1" class="mx-auto">
 						<div
-							class="bg-fill md:bg-center bg-center-left  md:h-screen  text-white py-24 px-10 object-fill"
+							class="bg-fill bg-center h-screen w-screen  text-white py-24 px-10 object-fill"
 							style="background-image: url(https://lh3.googleusercontent.com/905j9pNEx6GgQXirhPXagnnNDtsiULLPIDENga-CXxMqWRKQMYnJc1oiQi8DUDRWtdSfYYBsNomsYxJsnDgLhimpVthlhp-eDCyu2hZF-RN8L3a8oBk82zbOQjzsqs-6HITOLhpMig=w2400)"
 						>
 							<div
