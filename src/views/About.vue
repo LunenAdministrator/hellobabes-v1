e59c88aa2a7b2209a9af6940873ce272ebeb86e6
Lunen Developer <71112248+LunenAdministrator@users.noreply.github.com>
2021-06-05 08:31:33 +0800

    Update update map

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
 					marginheight="0"
 					marginwidth="0"
 					scrolling="no"
-					src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
+					src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=setia%20alam%20&t=&z=13&ie=UTF8&iwloc=&output=embed"
 					style="filter: grayscale(1) contrast(1.2) opacity(0.4);"
 				></iframe>
 				<div
